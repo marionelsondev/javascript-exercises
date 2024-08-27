8ad61b85ad4be65b28cb3df7f372c36c31126e9f
Mario Nelson <gbdev03@gbringel.com>
2024-08-27 17:25:06 -0300

    Adding ex001

---
+++ ex001/script.js
@@ -0,0 +1,15 @@
+// 1. Create a variable to store the car's name and set its value to "Ferrari" Then, display it.
+
+let carName = "Ferrari"
+console.log(carName)
+
+// 2. Create a variable to store the price of the cake, which costs $19.99. Then, display it.
+
+let cakePrice = 19.99
+console.log(`$${cakePrice}`)
+
+// 3. Correct the following code (and display it) 
+// let "city" = Sao Paulo
+
+let city = "Sao Paulo"
+console.log(city)
